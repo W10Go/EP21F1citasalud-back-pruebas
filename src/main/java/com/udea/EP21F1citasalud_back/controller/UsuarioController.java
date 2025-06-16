ec217e1c4f76c94fde96ddabdaa0e9cec723b1d9
W10Go <diegoa.carvajal@udea.edu.co>
2025-06-16 13:36:00 -0500

    Deleting the Preauthorize for the createUser function deletin unused import

--- src/main/java/com/udea/EP21F1citasalud_back/controller/UsuarioController.java
+++ src/main/java/com/udea/EP21F1citasalud_back/controller/UsuarioController.java
@@ -24,7 +24,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
 
 @RestController
